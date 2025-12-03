33e2847f684e138dc45f45affffd1fac149c8263
Matti <m.al-hussan@lyon.ort.asso.fr>
2025-12-03 14:20:44 +0100

    Ajout du style CSS pour le plateau de jeu et mise en place des grilles d'affichage et d'interaction

--- src/app/plateau/plateau.ts
+++ src/app/plateau/plateau.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-plateau',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './plateau.html',
   styleUrl: './plateau.css',
 })
 export class Plateau {
-
+  onCaseClick(i: number, j: number) {
+    // Action à effectuer lors d'un clic sur une case (à compléter selon la logique du jeu)
+    console.log('Case cliquée :', i, j);
+  }
 }
