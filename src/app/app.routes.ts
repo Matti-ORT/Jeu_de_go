247f99ebd39cc5f9c2c12a3cd9cf08fbb6131762
Matti <m.al-hussan@lyon.ort.asso.fr>
2025-12-03 11:35:27 +0100

    Ajout de la navigation et des routes pour le plateau et les cellules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Plateau } from './plateau/plateau';
+import { Cellules } from './cellules/cellules';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'plateau', pathMatch: 'full' },
+  { path: 'plateau', component: Plateau },
+  { path: 'cellules', component: Cellules },
+];
